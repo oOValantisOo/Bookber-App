852368363183ef7f0ef280c80afd7acd8754d36f
oOValantisOo <jakendra.lathaniel@gmail.com>
2024-12-17 16:31:56 +0700

    Include Vite build assets for deployment

---
+++ public/build/assets/bookber-Cz8C9FA6.js
@@ -0,0 +1,1 @@
+setTimeout(()=>{const t=document.getElementById("notification");t&&(t.style.transition="opacity 0.5s ease",t.style.opacity="0",setTimeout(()=>t.remove(),500))},3e3);
--- resources/views/home/home-guest.blade.php
+++ resources/views/home/home-guest.blade.php
@@ -3,6 +3,20 @@
 @section('title1', 'HomeG')
 
 @section('content1')
+
+@vite([
+        'resources/css/bootstrap.min.css',
+        'resources/css/bookber.css',
+        'resources/css/owl.css',
+        'resources/css/animate.css',
+        'resources/js/jquery.min.js',
+        'resources/js/custom.js',
+        'resources/js/bookber.js',
+        'resources/js/bootstrap.min.js'
+    ])
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 <main>
 
     @if(session()->has('success'))
