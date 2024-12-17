b117ac01be95e66f548917beb23e09905b81f04e
oOValantisOo <jakendra.lathaniel@gmail.com>
2024-12-17 16:11:47 +0700

    build

--- resources/views/layouts1/default.blade.php
+++ resources/views/layouts1/default.blade.php
@@ -7,29 +7,20 @@
     @vite([
         'resources/css/bootstrap.min.css',
         'resources/css/bookber.css',
+        'resources/css/owl.css',
+        'resources/css/animate.css',
         'resources/js/jquery.min.js',
-        'resources/js/custom.js'
+        'resources/js/custom.js',
+        'resources/js/bookber.js',
+        'resources/js/bootstrap.min.js'
     ])
-
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}"> --}}
-    <link rel="stylesheet" href="{{ asset('css/bookber.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     @include('components.navbar')
     <title>@yield('title1', 'Project Bookber1')</title>
 </head>
 <body>
     @yield('content1')
-    <script src="{{ asset('js/bookber.js') }}"></script>
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/isotope.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/owl-carousel.js') }}"></script>
-    <script src="{{ asset('js/counter.js') }}"></script>
-    <script src="{{ asset('js/custom.js') }}"></script>
 </body>
 </html>
