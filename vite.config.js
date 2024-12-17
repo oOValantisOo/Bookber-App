25ec04b7e2150f8b64ef04696180dd01b191f5a0
oOValantisOo <jakendra.lathaniel@gmail.com>
2024-12-17 16:36:39 +0700

    inline aja la

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/bootstrap.min.css',
-                'resources/css/bookber.css',
-                'resources/css/owl.css',
-                'resources/css/animate.css',
-                'resources/css/css-login-register/login-register.css', 
-                'resources/js/bookber.js',
-                'resources/js/bootstrap.min.js',
-                'resources/js/jquery.min.js',
-                'resources/js/custom.js',
-                'resources/js/js-login-register/counter.js',
-                'resources/js/js-login-register/isotope.min.js',
-                'resources/js/js-login-register/jquery.min.js',
-                'resources/js/js-login-register/login-register.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
